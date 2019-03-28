55228b1c5738afb042e33d4fb7cc29f175cd1a78
AlexY <alexyo87@yahoo.com>
2019-03-28 14:33:35 +0300

    Updates

---
+++ precache-manifest.cacbab586d601a4245b3f8134a7bce5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed2975c6e24435f3ef2e",
+    "url": "/face-info-detector/static/js/runtime~main.c4827bcd.js"
+  },
+  {
+    "revision": "6e022d6ced3af964ca92",
+    "url": "/face-info-detector/static/js/main.ff2876a4.chunk.js"
+  },
+  {
+    "revision": "779f3b849883f29d3d01",
+    "url": "/face-info-detector/static/js/2.123542bf.chunk.js"
+  },
+  {
+    "revision": "6e022d6ced3af964ca92",
+    "url": "/face-info-detector/static/css/main.d9dbc699.chunk.css"
+  },
+  {
+    "revision": "779f3b849883f29d3d01",
+    "url": "/face-info-detector/static/css/2.170b55b2.chunk.css"
+  },
+  {
+    "revision": "ee4fcc97efae03bc3eb5ebd7bffbaaf3",
+    "url": "/face-info-detector/index.html"
+  }
+];

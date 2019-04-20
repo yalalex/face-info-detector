60da7d810dd824fe42b2a01e2409335b7238e09e
AlexY <alexyo87@yahoo.com>
2019-04-20 14:23:49 +0300

    infobox edit

--- src/FaceRecognition/Box.js
+++ src/FaceRecognition/Box.js
@@ -3,8 +3,8 @@ import './FaceRecognition.css';
 
 const Box = ({ box }) => {
 	return (
-		<div className='bounding-box showhim' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
-			<div className='showme'>
+		<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+			<div className='showinfo'>
 				<p className='inthebox'>{`Sex: ${box.gender}`}</p>
 				<p className='inthebox'>{`Age: ${box.age}`}</p>
 				<p className='inthebox'>{`Race: ${box.race}`}</p>

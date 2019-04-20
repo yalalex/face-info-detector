2ef09a7ba3bc7a4eba67b1da0aeb86aef9e71765
AlexY <alexyo87@yahoo.com>
2019-04-20 14:26:23 +0300

    Updates

--- precache-manifest.053196224f2013514d18860e472ddcc2.js
+++ precache-manifest.053196224f2013514d18860e472ddcc2.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/face-info-detector/static/js/runtime~main.c4827bcd.js"
   },
   {
-    "revision": "46831e56786ca04e2020",
-    "url": "/face-info-detector/static/js/main.e8edddd7.chunk.js"
+    "revision": "fdbc2cff12b683aaef84",
+    "url": "/face-info-detector/static/js/main.98ec518c.chunk.js"
   },
   {
     "revision": "779f3b849883f29d3d01",
     "url": "/face-info-detector/static/js/2.123542bf.chunk.js"
   },
   {
-    "revision": "46831e56786ca04e2020",
-    "url": "/face-info-detector/static/css/main.d9dbc699.chunk.css"
+    "revision": "fdbc2cff12b683aaef84",
+    "url": "/face-info-detector/static/css/main.0287caf1.chunk.css"
   },
   {
     "revision": "779f3b849883f29d3d01",
     "url": "/face-info-detector/static/css/2.170b55b2.chunk.css"
   },
   {
-    "revision": "d1b7296e799b3c5f269a188487d202ea",
+    "revision": "509bc13e168b0caa3416649ba0aba8b1",
     "url": "/face-info-detector/index.html"
   }
 ];

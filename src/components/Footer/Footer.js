9308353defd65b8d9164493e7cad983d85c1f640
AlexY <alexyo87@yahoo.com>
2019-06-25 14:14:09 +0300

    editing footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>© 2019 <a className="link dim black pointer" href="https://github.com/yalalex">Alex Y</a>.
+    </footer>
+  );
+}
+
+export default Footer;

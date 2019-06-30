85e87d5a43de1d6dcb67033a1db47f2c5156c3f8
AlexY <alexyo87@yahoo.com>
2019-06-30 14:21:31 +0300

    footer fix

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ import Box from './Box';
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
-      <div className='absolute mt2'>
+      <div className='absolute mv2'>
         <img id='inputimage' alt='' src={imageUrl} width='480px' heigh='auto' />
         {box.map((face, i) => {
           return <Box key={i} box={box[i]} />;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './Footer.css';
-
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer className='tr fixed bottom-0 w-100'>
       © 2019{' '}
       <a className='link dim black pointer' href='https://github.com/yalalex'>
         Alex Y

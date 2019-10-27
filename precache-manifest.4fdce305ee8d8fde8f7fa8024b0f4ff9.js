d395292804a0f40923a53db315270bb58311102a
AlexY <alexyo87@yahoo.com>
2019-10-27 18:03:47 +0300

    Updates

--- precache-manifest.4fdce305ee8d8fde8f7fa8024b0f4ff9.js
+++ precache-manifest.4fdce305ee8d8fde8f7fa8024b0f4ff9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/face-info-detector/static/js/runtime~main.c4827bcd.js"
   },
   {
-    "revision": "85569079fd80720d69fc",
-    "url": "/face-info-detector/static/js/main.d363a063.chunk.js"
+    "revision": "f86d54b9aadcbaad5810",
+    "url": "/face-info-detector/static/js/main.4990b7f3.chunk.js"
   },
   {
     "revision": "cd4aae6ff154cc65e142",
     "url": "/face-info-detector/static/js/2.f8a8c402.chunk.js"
   },
   {
-    "revision": "85569079fd80720d69fc",
+    "revision": "f86d54b9aadcbaad5810",
     "url": "/face-info-detector/static/css/main.d29a50d4.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/face-info-detector/static/css/2.6ea6da10.chunk.css"
   },
   {
-    "revision": "f5f7737ead690eefdae924024018089c",
+    "revision": "c30aea2a1b80c30705033ab64aac2296",
     "url": "/face-info-detector/index.html"
   }
 ];
